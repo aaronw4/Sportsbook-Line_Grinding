554971b6fb5b878f1967bacf317d6040f17a017c
Aaron Williams <aaronrw4@yahoo.com>
2020-03-12 08:35:24 -0700

    Imported ncaaBB into App.js

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NcaaBB from './components/ncaaBB'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NcaaBB/>
     </div>
   );
 }
