421c5469a572344d45aa8cbeccc722dae4d3ff30
Aaron Williams <aaronrw4@yahoo.com>
2020-03-12 09:09:46 -0700

    Fixed typo in axiosWithAuth

--- sportsbook/src/components/axiosWithAuth.js
+++ sportsbook/src/components/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
     return axios.create({
-        baseURL: 'api-basketball.p.rapidapi.com',
+        baseURL: 'https://api-basketball.p.rapidapi.com',
         headers: {
             'x-rapidapi-host': 'api-basketball.p.rapidapi.com',
 	        'x-rapidapi-key': '9e7abdd2a5msh27d1e47d918d27ap1b7b0fjsneed4da8f01d5'
