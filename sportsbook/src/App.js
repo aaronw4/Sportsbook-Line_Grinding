7ba8b86a984c88fc273bd5a53a50572c43b036eb
Aaron Williams <aaronrw4@yahoo.com>
2020-03-15 01:29:13 -0700

    Saved data to state and passed down as prop

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -10,13 +10,14 @@ import './App.css';
 
 function App() {
    // const [date, setDate] = useState(new Date().toISOString().slice(0,10));
+   const [games, setGames] = useState([]);
    const date = '2020-03-10';
   
   function fetchNcaabb() {
       axiosWithAuthNcaabb()
           .get(`/sports/5/events/${date}?include=all_periods&include=scores&offset=0`)            
           .then(res => {
-            localStorage.setItem('ncaabb', res.data.events)
+            setGames(res.data.events)
             console.log(res.data.events);
           })
           .catch(err=> console.log(err))
@@ -25,25 +26,27 @@ function App() {
   return (
     <div className="App">
       <Route exact path='/'>
-        <Link to='/ncaabb' onClick={fetchNcaabb}>
-          <h3>NCAA BB</h3>
-        </Link>
-        <Link to='/ncaafb'>
-          <h3>NCAA FB</h3>
-        </Link>
-        <Link to='/nfl'>
-          <h3>NFL</h3>
-        </Link>
-        <Link path='/nba'>
-          <h3>NBA</h3>
-        </Link>
-        <Link path='/mlb'>
-          <h3>MLB</h3>
-        </Link>
+        <div className='homePageLinks'>
+          <Link onClick={fetchNcaabb} to='/ncaabb'>
+            <h2 className='homePageLink'>NCAA BB</h2>
+          </Link>
+          <Link to='/ncaafb'>
+            <h2>NCAA FB</h2>
+          </Link>
+          <Link to='/nfl'>
+            <h2>NFL</h2>
+          </Link>
+          <Link path='/nba'>
+            <h2>NBA</h2>
+          </Link>
+          <Link path='/mlb'>
+            <h2>MLB</h2>
+          </Link>
+        </div>
       </Route>
 
       <Route path='/ncaabb'>
-        <NcaaBB/>
+        <NcaaBB games={games}/>
       </Route>
 
       <Route path='/ncaafb'>
