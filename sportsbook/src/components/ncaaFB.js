2c39cb7f0479843caa093a9a8292de022ea389ce
Aaron Williams <aaronrw4@yahoo.com>
2020-03-19 20:55:49 -0700

    Pulled in games data and diplays teams playing

--- sportsbook/src/components/ncaaFB.js
+++ sportsbook/src/components/ncaaFB.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const NcaaFB = () => {
+const NcaaFB = (props) => {
+    const [pinn, setPinn] = useState([]);
+
+    let games = props.games;
+
+    useEffect(() => {
+        const pinnacle = games.map(game => 
+            game.line_periods[3]    
+        );
+        const pinny = pinnacle.filter(line => 
+            line != null    
+        );
+        setPinn(pinny)
+    },[games])
 
     return (
-        <div>
+        <div className='lines'>
+            <div>
+                {games.map(game => (
+                    <div className='matchup'>
+                        <h5>Teams</h5>
+                        <p>{game.teams_normalized[0].name}</p>
+                        <p>{game.teams_normalized[1].name}</p>
+                    </div>
+                ))}
+            </div>
 
         </div>
     )
