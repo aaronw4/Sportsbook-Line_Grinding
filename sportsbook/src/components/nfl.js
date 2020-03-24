5e7bfa15e0f23d10f534e99d5f021b9942074695
Aaron Williams <aaronrw4@yahoo.com>
2020-03-23 20:08:48 -0700

    Displays team match-ups

--- sportsbook/src/components/nfl.js
+++ sportsbook/src/components/nfl.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const NFL = () => {
+const NFL = (props) => {
+    let games = props.games;
 
     return (
-        <div>
-
+        <div className='lines'>
+            <div>
+                {games.map(game => (
+                    <div className='matchup'>
+                        <h5>Teams</h5>
+                        <p>{game.teams_normalized[0].name}</p>
+                        <p>{game.teams_normalized[1].name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
