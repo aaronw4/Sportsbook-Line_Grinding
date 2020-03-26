243bf9c8fc6b00892fb66cb605f72794beb58a7c
Aaron Williams <aaronrw4@yahoo.com>
2020-03-25 22:40:46 -0700

    Display matchups in mlb.js

--- sportsbook/src/components/mlb.js
+++ sportsbook/src/components/mlb.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const MLB = () => {
+    let games = props.games;
 
     return (
         <div>
-
+            <div>
+                {games.map(game => (
+                    <div className='matchup'>
+                        <h5>Teams</h5>
+                        <p>{game.teams_normalized[0].name}</p>
+                        <p>{game.teams_normalized[1].name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
