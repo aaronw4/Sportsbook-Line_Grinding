cfa2e7672d4ea918b3f782b6e94a32080286dbad
Aaron Williams <aaronrw4@yahoo.com>
2020-03-31 22:00:46 -0700

    Added useEffect  condition for midpoint

--- sportsbook/src/components/nba.js
+++ sportsbook/src/components/nba.js
@@ -87,7 +87,7 @@ const NBA = (props) => {
             let mp = (1 / recRatio) * 100;
             return mp
         }
-    },[pinn]);
+    },[pinn, gamePeriod]);
     
     function integer(number) {
         let num = Number(number);
