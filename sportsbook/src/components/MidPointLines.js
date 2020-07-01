a37993fce20c5183b10037bf735373fb73e37d2f
Aaron Williams <aaronrw4@yahoo.com>
2020-06-30 21:19:11 -0700

    Created MidPointLines component

---
+++ sportsbook/src/components/MidPointLines.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const MidPointLines = (props) => {
+    const midPoint = props.midPoint
+
+    function integer(number) {
+        let num = Number(number);
+        let integer = num.toFixed(0);
+        return integer;
+    };
+    
+    return(
+        <div>
+            {midPoint.map(line => (
+                <div className='bets'> 
+                    <div className='bet'>
+                        <h5>Moneyline</h5>
+                        <p>Midpoint: {integer(line.moneylineMP)}</p>
+                        <p>Win Rate: {integer(line.moneylineMP/(line.moneylineMP + 100) * 100)}</p>
+                    </div>
+                    <div className='bet'>
+                        <h5>Spread</h5>
+                        <p>Midpoint: {integer(line.spreadMP)}</p>
+                        <p>Win Rate: {integer(line.spreadMP/(line.spreadMP + 100) * 100)}</p>
+                    </div>
+                    <div>
+                        <h5>Total</h5>
+                        <p>Midpoint: {integer(line.totalMP)}</p>
+                        <p>Win Rate: {integer(line.totalMP/(line.totalMP + 100) * 100)}</p>
+                    </div>
+                </div>
+            ))}
+        </div>     
+    )
+};
+
+export default MidPointLines
