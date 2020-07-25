e80221ae92199628df2e644180567549e5bcb013
Aaron Williams <aaronrw4@yahoo.com>
2020-07-25 00:36:31 -0700

    Fix MinValue to display correct odds

--- sportsbook/src/components/MinValue.js
+++ sportsbook/src/components/MinValue.js
@@ -10,14 +10,14 @@ const MinValue = (props) => {
     }
 
     const favOdds = mp => {
-        let wr = integer(mp / (mp + 100) * 100);
-        let line = -100 / (100 / wr - 1)
+        let wr = (mp / (mp + 100) * 100);
+        let line = integer(-100 / (100 / wr - 1))
         return line
     }
 
     const dogOdds = mp => {
-        let wr = integer(mp / (mp + 100) * 100);
-        let line = ((1 / (wr / 100 + 1)) - 1) * 100;
+        let wr = (mp / (mp + 100) * 100);
+        let line = integer(((1 / (-1 * wr / 100 + 1)) - 1) * 100);
         return line
     }
     
@@ -26,19 +26,19 @@ const MinValue = (props) => {
             {midPoint.map(line => (
                 <div className='bets'>
                     <div className='bet'>
-                        <h5>Moneyline</h5>
-                        <p>Favorite: {() => favOdds(line.moneylineMP)}</p>
-                        <p>Dog: {() => dogOdds(line.moneylineMP)}</p>
+                        <h5>.</h5>
+                        <p>Favorite: {favOdds(line.moneylineMP)}</p>
+                        <p>Dog: {dogOdds(line.moneylineMP)}</p>
                     </div>
                     <div className='bet'>
-                        <h5>Spread</h5>
-                        <p>Favorite: {() => favOdds(line.spreadMP)}</p>
-                        <p>Dog: {() => dogOdds(line.spreadMP)}</p>
+                        <h5>.</h5>
+                        <p>Favorite: {favOdds(line.spreadMP)}</p>
+                        <p>Dog: {dogOdds(line.spreadMP)}</p>
                     </div>
                     <div>
-                        <h5>Total</h5>
-                        <p>Over: {() => favOdds(line.totalMP)}</p>
-                        <p>Under: {() => dogOdds(line.totalMP)}</p>
+                        <h5>.</h5>
+                        <p>Over: {favOdds(line.totalMP)}</p>
+                        <p>Under: {dogOdds(line.totalMP)}</p>
                     </div>
                 </div>
             ))}
