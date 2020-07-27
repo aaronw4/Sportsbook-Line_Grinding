7777c41f855e11514e0099bb6ad70a78b9d37924
Aaron Williams <aaronrw4@yahoo.com>
2020-07-27 03:09:53 -0700

    Added game ID to game match-up

--- sportsbook/src/components/MatchUp.js
+++ sportsbook/src/components/MatchUp.js
@@ -6,10 +6,19 @@ const MathchUp = (props) => {
     return(
         <div>
             {games.map(game => (
-                <div className='matchup'>
+                game.line_periods[3] == null ?
+                <div className='matchup' key={game.event_id}>
                     <h5>Teams</h5>
                     <p>{game.teams_normalized[0].name}</p>
                     <p>{game.teams_normalized[1].name}</p>
+                    <br/>
+                </div>
+                :
+                <div className='matchup' key={game.event_id}>
+                    <h5>Teams</h5>
+                    <p>{game.teams_normalized[0].name}</p>
+                    <p>{game.teams_normalized[1].name}</p>
+                    <p>{game.line_periods[3].period_full_game.moneyline.line_id}</p>
                 </div>
             ))}
         </div>
