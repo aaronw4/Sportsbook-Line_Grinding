0f5f2238d016ddb6fdf5e58329e9d0ab5a417e90
Aaron Williams <aaronrw4@yahoo.com>
2020-07-29 14:52:59 -0700

    Reformatted display to show away/home team odds

--- sportsbook/src/components/MinValue.js
+++ sportsbook/src/components/MinValue.js
@@ -24,26 +24,48 @@ const MinValue = (props) => {
     return(
         <div>
             <br/><br/>
-            {midPoint.map(line => (
+            {midPoint.map(line => 
                 <div className='bets' key={line.id}>
+                    {line.away === 'Dog' ?
                     <div className='bet'>
                         <br className='minValueBreak topHeader'/>
-                        <p>Favorite: {favOdds(line.moneylineMP)}</p>
-                        <p>Dog: {dogOdds(line.moneylineMP)}</p>
+                        <p>Away: {dogOdds(line.moneylineMP)}</p>
+                        <p>Home: {favOdds(line.moneylineMP)}</p>
                         <p>{line.id}</p>
                     </div>
+                    :
                     <div className='bet'>
                         <br className='minValueBreak topHeader'/>
-                        <p>Favorite: {favOdds(line.spreadMP)}</p>
-                        <p>Dog: {dogOdds(line.spreadMP)}</p>
+                        <p>Away: {favOdds(line.moneylineMP)}</p>
+                        <p>Home: {dogOdds(line.moneylineMP)}</p>
+                        <p>{line.id}</p>
+                    </div>}
+                    {line.awaySpread === 'Dog' ?
+                    <div className='bet'>
+                        <br className='minValueBreak midHeader'/>
+                        <p>Away: {dogOdds(line.spreadMP)}</p>
+                        <p>Home: {favOdds(line.spreadMP)}</p>
                     </div>
+                    :
+                    <div className='bet'>
+                        <br className='minValueBreak midHeader'/>
+                        <p>Away: {favOdds(line.spreadMP)}</p>
+                        <p>Home: {dogOdds(line.spreadMP)}</p>
+                    </div>}
+                    {line.favorite === 'Over' ?
                     <div>
-                        <br className='minValueBreak topHeader'/>
-                        <p>Favorite: {favOdds(line.totalMP)}</p>
-                        <p>Dog: {dogOdds(line.totalMP)}</p>
+                        <br className='minValueBreak midHeader'/>
+                        <p>Over: {favOdds(line.totalMP)}</p>
+                        <p>Under: {dogOdds(line.totalMP)}</p>
                     </div>
+                    :
+                    <div>
+                        <br className='minValueBreak midHeader'/>
+                        <p>Over: {dogOdds(line.totalMP)}</p>
+                        <p>Under: {favOdds(line.totalMP)}</p>
+                    </div>}
                 </div>
-            ))}
+            )}
         </div>
     )
 }
