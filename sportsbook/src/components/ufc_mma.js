1198e5ab0b134b48bd8bb6ce3cb82e13d4995e87
Aaron Williams <aaronrw4@yahoo.com>
2020-08-08 02:04:11 -0700

    Created ufc page

---
+++ sportsbook/src/components/ufc_mma.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import {MidPoint} from './midPoint';
+import MathchUp from './MatchUp';
+import MidPointLines from './MidPointLines';
+import Edge from './Edge';
+import Lines from './Lines';
+import MinValue from './MinValue'
+
+const UFC_MMA = (props) => {
+    const [pinn, setPinn] = useState([]);
+    const [midPoint, setMidPoint] = useState([]); 
+
+    let games = props.games;
+    let date = props.date;
+
+    useEffect(() => {
+        const pinnacle = games.map(game => 
+            game.line_periods[3]    
+        );
+        const pinny = pinnacle.filter(line => 
+            line != null    
+        );
+        setPinn(pinny)
+    },[games]);
+
+    useEffect(() => {
+        setMidPoint(MidPoint('fullGame', pinn, 0));
+    },[pinn]);
+
+    return (
+        <div>
+            {console.log(midPoint)}
+            <div className='sportTitle'>
+                <h3>UFC_MMA Lines</h3> 
+            </div>
+            <div className='lines'>
+                <MathchUp games={games} date={date} game='MMA'/>
+                <Lines 
+                    gamePeriod='MMA'
+                    pinn={pinn} 
+                    halfPoint={0}
+                    game='MMA'
+                />
+                <MidPointLines midPoint={midPoint} gamePeriod='MMA'/>
+                <MinValue midPoint={midPoint} gamePeriod='MMA'/>   
+                <Edge/>
+            </div>
+        </div>
+    )
+}
+
+export default UFC_MMA
