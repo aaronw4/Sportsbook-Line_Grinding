c225f6fee1b40835192ebf8873001339c7a9ac15
Aaron Williams <aaronrw4@yahoo.com>
2020-08-08 02:06:04 -0700

    Created MMA midpoint lines

--- sportsbook/src/components/MidPointLines.js
+++ sportsbook/src/components/MidPointLines.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const MidPointLines = (props) => {
     const midPoint = props.midPoint
+    const gamePeriod = props.gamePeriod
 
     function integer(number) {
         let num = Number(number);
@@ -12,26 +13,36 @@ const MidPointLines = (props) => {
     return(
         <div>
             <br/><br/>
-            {midPoint.map(line => (
-                <div className='bets' key={line.id}> 
-                    <div>
-                        <h5 className='topHeader'>Spread</h5>
-                        <p>Midpoint: {integer(line.spreadMP)}</p>
-                        <p>Win Rate: {integer(line.spreadMP/(line.spreadMP + 100) * 100)}</p>
-                    </div>
-                    <div>
+            {gamePeriod === 'MMA' ?
+                midPoint.map(line => (
+                    <div className='betsMMA'>
                         <h5>Moneyline</h5>
                         <p>Midpoint: {integer(line.moneylineMP)}</p>
                         <p>Win Rate: {integer(line.moneylineMP/(line.moneylineMP + 100) * 100)}</p>
                         <p>{line.id}</p>
                     </div>
-                    <div>
-                        <h5>Total</h5>
-                        <p>Midpoint: {integer(line.totalMP)}</p>
-                        <p>Win Rate: {integer(line.totalMP/(line.totalMP + 100) * 100)}</p>
+                )) :
+                midPoint.map(line => (
+                    <div className='bets' key={line.id}> 
+                        <div>
+                            <h5 className='topHeader'>Spread</h5>
+                            <p>Midpoint: {integer(line.spreadMP)}</p>
+                            <p>Win Rate: {integer(line.spreadMP/(line.spreadMP + 100) * 100)}</p>
+                        </div>
+                        <div>
+                            <h5>Moneyline</h5>
+                            <p>Midpoint: {integer(line.moneylineMP)}</p>
+                            <p>Win Rate: {integer(line.moneylineMP/(line.moneylineMP + 100) * 100)}</p>
+                            <p>{line.id}</p>
+                        </div>
+                        <div>
+                            <h5>Total</h5>
+                            <p>Midpoint: {integer(line.totalMP)}</p>
+                            <p>Win Rate: {integer(line.totalMP/(line.totalMP + 100) * 100)}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))
+            }
         </div>     
     )
 };
