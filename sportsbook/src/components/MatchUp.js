9b531777f286b55dac65cc9773699f448fd6f247
Aaron Williams <aaronrw4@yahoo.com>
2020-08-08 02:05:01 -0700

    created lines for matchup

--- sportsbook/src/components/MatchUp.js
+++ sportsbook/src/components/MatchUp.js
@@ -21,6 +21,20 @@ const MathchUp = (props) => {
                 </div>
             ))}
         </div>
+        : props.game === 'MMA' ?
+        <div>
+            <p className='date'>{date}</p>
+            {games.map(game => (
+                game.line_periods[3] == null ? null :
+                <div className='matchupMMA' key={game.event_id}>
+                    <h5>Match</h5>
+                    <p>{game.teams_normalized[0].name}</p>
+                    <p>{game.teams_normalized[1].name}</p>
+                    <p>{game.line_periods[3].period_full_game.moneyline.line_id}</p>
+                    <p>{game.score.event_status_detail}</p>
+                </div>
+            ))}
+        </div>
         :
         <div>
             <p className='date'>{date}</p>
