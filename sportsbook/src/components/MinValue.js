eba6f1aefd8f1f6f885e2828c9163fab809ad24c
Aaron Williams <aaronrw4@yahoo.com>
2020-08-08 02:06:26 -0700

    Created minValue lines

--- sportsbook/src/components/MinValue.js
+++ sportsbook/src/components/MinValue.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const MinValue = (props) => { 
     const midPoint = props.midPoint;
+    const gamePeriod = props.gamePeriod
     const [n, setNumber] = useState(1)
 
     function integer(number) {
@@ -18,50 +19,71 @@ const MinValue = (props) => {
     return(
         <div>
             <br/><br/>
-            {midPoint.map(line => 
-                <div className='bets' key={line.id}>
-                    {line.awaySpread === 'Dog' ?
-                    <div>
-                        <br className='minValueBreak topHeader'/>
-                        <p>Away: {n * integer(line.spreadMP)}</p>
-                        <p>Home: {-n * integer(line.spreadMP)}</p>
-                        <button onClick={() => changeSign()}>+ / -</button>
+            {gamePeriod === 'MMA' ?
+                midPoint.map(line => 
+                    <div className='betsMMA' key={line.id}>
+                        {line.away === 'Dog' ?
+                        <div>
+                            <br className='minValueBreak mmaHeader'/>
+                            <p>Away: {integer(line.moneylineMP)}</p>
+                            <p>Home: -{integer(line.moneylineMP)}</p>
+                            <p>{line.id}</p>
+                        </div>
+                        :
+                        <div>
+                            <br className='minValueBreak mmaHeader'/>
+                            <p>Away: -{integer(line.moneylineMP)}</p>
+                            <p>Home: {integer(line.moneylineMP)}</p>
+                            <p>{line.id}</p>
+                        </div> 
+                        }
+                    </div>                  
+                ) :
+                midPoint.map(line => 
+                    <div className='bets' key={line.id}>
+                        {line.awaySpread === 'Dog' ?
+                        <div>
+                            <br className='minValueBreak topHeader'/>
+                            <p>Away: {n * integer(line.spreadMP)}</p>
+                            <p>Home: {-n * integer(line.spreadMP)}</p>
+                            <button onClick={() => changeSign()}>+ / -</button>
+                        </div>
+                        :
+                        <div>
+                        <   br className='minValueBreak topHeader'/>
+                            <p>Away: {-n * integer(line.spreadMP)}</p>
+                            <p>Home: {n * integer(line.spreadMP)}</p>
+                            <button onClick={() => changeSign()}>+ / -</button>
+                        </div>}
+                        {line.away === 'Dog' ?
+                        <div>
+                            <br className='minValueBreak midHeader'/>
+                            <p>Away: {integer(line.moneylineMP)}</p>
+                            <p>Home: -{integer(line.moneylineMP)}</p>
+                            <p>{line.id}</p>
+                        </div>
+                        :
+                        <div>
+                            <br className='minValueBreak midHeader'/>
+                            <p>Away: -{integer(line.moneylineMP)}</p>
+                            <p>Home: {integer(line.moneylineMP)}</p>
+                            <p>{line.id}</p>
+                        </div>}
+                        {line.favorite === 'Over' ?
+                        <div>
+                            <br className='minValueBreak bottomHeader'/>
+                            <p>Over: -{integer(line.totalMP)}</p>
+                            <p>Under: {integer(line.totalMP)}</p>
+                        </div>
+                        :
+                        <div>
+                            <br className='minValueBreak bottomHeader'/>
+                            <p>Over: {integer(line.totalMP)}</p>
+                            <p>Under: -{integer(line.totalMP)}</p>
+                        </div>}
                     </div>
-                    :
-                    <div>
-                    <   br className='minValueBreak topHeader'/>
-                        <p>Away: {-n * integer(line.spreadMP)}</p>
-                        <p>Home: {n * integer(line.spreadMP)}</p>
-                        <button onClick={() => changeSign()}>+ / -</button>
-                    </div>}
-                    {line.away === 'Dog' ?
-                    <div>
-                        <br className='minValueBreak midHeader'/>
-                        <p>Away: {integer(line.moneylineMP)}</p>
-                        <p>Home: -{integer(line.moneylineMP)}</p>
-                        <p>{line.id}</p>
-                    </div>
-                    :
-                    <div>
-                        <br className='minValueBreak midHeader'/>
-                        <p>Away: -{integer(line.moneylineMP)}</p>
-                        <p>Home: {integer(line.moneylineMP)}</p>
-                        <p>{line.id}</p>
-                    </div>}
-                    {line.favorite === 'Over' ?
-                    <div>
-                        <br className='minValueBreak bottomHeader'/>
-                        <p>Over: -{integer(line.totalMP)}</p>
-                        <p>Under: {integer(line.totalMP)}</p>
-                    </div>
-                    :
-                    <div>
-                        <br className='minValueBreak bottomHeader'/>
-                        <p>Over: {integer(line.totalMP)}</p>
-                        <p>Under: -{integer(line.totalMP)}</p>
-                    </div>}
-                </div>
-            )}
+                )
+            }
         </div>
     )
 }
