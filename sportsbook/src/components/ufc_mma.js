188ce63adfa4bfc18a69d09121904175a621be02
Aaron Williams <aaronrw4@yahoo.com>
2020-09-19 17:28:08 -0700

    Reformatted ufc mma

--- sportsbook/src/components/ufc_mma.js
+++ sportsbook/src/components/ufc_mma.js
@@ -1,35 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {MidPoint} from './functions/midPoint';
-import MathchUp from './lines/MatchUp';
-import MidPointLines from './lines/MidPointLines';
+import React from 'react';
 import Edge from './Edge';
-import Lines from './lines/Lines';
-import MinValue from './lines/MinValue'
+import GameCards from './gameCards';
 
 const UFC_MMA = (props) => {
-    const [pinn, setPinn] = useState([]);
-    const [midPoint, setMidPoint] = useState([]); 
-
     let games = props.games;
-    let date = props.date;
-
-    useEffect(() => {
-        const pinnacle = games.map(game => 
-            game.line_periods[3]    
-        );
-        const pinny = pinnacle.filter(line => 
-            line != null    
-        );
-        setPinn(pinny)
-    },[games]);
-
-    useEffect(() => {
-        setMidPoint(MidPoint('fullGame', pinn, 0));
-    },[pinn]);
 
     return (
         <div>
-            {console.log(midPoint)}
             <head>
                 <title>UFC / MMA</title>
             </head>
@@ -37,15 +14,12 @@ const UFC_MMA = (props) => {
                 <h3>UFC_MMA Lines</h3> 
             </div>
             <div className='lines'>
-                <MathchUp games={games} date={date} game='MMA'/>
-                <Lines 
+                <GameCards 
                     gamePeriod='MMA'
-                    pinn={pinn} 
+                    games={games} 
                     halfPoint={0}
                     game='MMA'
                 />
-                <MidPointLines midPoint={midPoint} gamePeriod='MMA'/>
-                <MinValue midPoint={midPoint} gamePeriod='MMA'/>   
                 <Edge/>
             </div>
         </div>
