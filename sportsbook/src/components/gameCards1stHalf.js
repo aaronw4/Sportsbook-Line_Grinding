15d0f6198767d304bc45d2fc0fd1f9fd9230417c
Aaron Williams <aaronrw4@yahoo.com>
2020-10-11 16:14:02 -0700

    Added code to handle baseball 1s5 5 innings

--- sportsbook/src/components/gameCards1stHalf.js
+++ sportsbook/src/components/gameCards1stHalf.js
@@ -7,6 +7,43 @@ const GameCards1stHalf = (props) => {
     const game = props.game
 
     return (
+        game === 'baseball' ?
+        <div>
+            {games.map(game => (
+                game.line_periods[3] == null ? null :
+                <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
+                    <div className="teams">
+                        <h5>Teams</h5>
+                        <p>{game.teams_normalized[0].name}</p>
+                        <p className='pitcher'>{game.pitcher_away.name}</p>
+                        <p>{game.teams_normalized[1].name}</p>
+                        <p className='pitcher'>{game.pitcher_home.name}</p>
+                        <p>{game.score.event_status_detail}</p>
+                    </div>
+                    <div className='betTypes'>
+                        <p className='betText'>Spread</p>
+                        <p className='betText'>Total</p>
+                    </div>
+                    <div className='currentLines'>
+                        <h5>Current</h5>
+                        <p>Away: {game.line_periods[3].period_first_half.spread.point_spread_away}({game.line_periods[3].period_first_half.spread.point_spread_away_money})</p>
+                        <p>Home: {game.line_periods[3].period_first_half.spread.point_spread_home}({game.line_periods[3].period_first_half.spread.point_spread_home_money})</p>
+                        <br/>
+                        <p>Over {game.line_periods[3].period_first_half.total.total_over}: {game.line_periods[3].period_first_half.total.total_over_money}</p>
+                        <p>Under {game.line_periods[3].period_first_half.total.total_over}: {game.line_periods[3].period_first_half.total.total_under_money}</p>
+                    </div>
+                    <div className='midpoints'>
+                        <h5>Starting</h5>
+                        <MidPointLines 
+                            total={game.line_periods[3].period_first_half.total.total_over}
+                            condition1={props.game}
+                            condition2=''
+                        />
+                    </div>
+                </div>
+            ))}
+        </div> 
+        :
         <div>
             {games.map(game => (
                 game.line_periods[3] == null ? null :
