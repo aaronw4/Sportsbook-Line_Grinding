9910fd3bb8fbaefc4c7a5af2f309a374b61363fa
Aaron Williams <aaronrw4@yahoo.com>
2020-10-24 17:06:37 -0700

    Updated axiosWithAuth with new API credentials

--- sportsbook/src/components/functions/axiosWithAuth.js
+++ sportsbook/src/components/functions/axiosWithAuth.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
     return axios.create({
-        baseURL: 'https://therundown-therundown-v1.p.rapidapi.com',
+        baseURL: 'https://sportspage-feeds.p.rapidapi.com',
         headers: {
-            "x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
-            "x-rapidapi-key": "9e7abdd2a5msh27d1e47d918d27ap1b7b0fjsneed4da8f01d5"
+            "x-rapidapi-host": "sportspage-feeds.p.rapidapi.com",
+		    "x-rapidapi-key": "9e7abdd2a5msh27d1e47d918d27ap1b7b0fjsneed4da8f01d5"
         }
     })
 }
