1ba282fcd1abcd501b0fb71020e9ba30eb97fb2c
Aaron Williams <aaronrw4@yahoo.com>
2020-10-24 17:54:25 -0700

    Updated location of object data

--- sportsbook/src/components/gameCards.js
+++ sportsbook/src/components/gameCards.js
@@ -21,7 +21,6 @@ const GameCards = (props) => {
         game === 'baseball' ?
         <div>
             {games.map(game => (
-                game.line_periods[3] == null ? null :
                 <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
                     <div className="teams">
                         <h5>Teams</h5>
@@ -57,7 +56,6 @@ const GameCards = (props) => {
         : game === 'MMA' ?
         <div>
             {games.map(game => (
-                game.line_periods[3] == null ? null :
                 <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
                     <div className='teams'>
                         <h5>Fighters</h5>
@@ -90,14 +88,13 @@ const GameCards = (props) => {
         </div>
         :
         <div>
-            {games.map(game => (
-                game.line_periods[3] == null ? null :
-                <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
+            {games.results.map(game => (
+                <div className='bets' key={game.gameId}>
                     <div className='teams'>
                         <h5>Teams</h5>
-                        <p>{game.teams_normalized[0].name}</p>
-                        <p>{game.teams_normalized[1].name}</p>
-                        <p>{game.score.event_status_detail}</p>
+                        <p>{game.teams.away.team}</p>
+                        <p>{game.teams.home.team}</p>
+                        <p>{game.status}</p>
                     </div>
                     <div className='betTypes'>
                         <p className='betText'>Spread</p>
@@ -106,18 +103,18 @@ const GameCards = (props) => {
                     </div>
                     <div className='currentLines'>
                         <h5>Current</h5>
-                        <p>Away: {game.line_periods[3].period_full_game.spread.point_spread_away}({game.line_periods[3].period_full_game.spread.point_spread_away_money})</p>
-                        <p>Home: {game.line_periods[3].period_full_game.spread.point_spread_home}({game.line_periods[3].period_full_game.spread.point_spread_home_money})</p>
+                        <p>Away: {game.odds[0].spread.current.away}({game.odds[0].spread.current.awayOdds})</p>
+                        <p>Home: {game.odds[0].spread.current.home}({game.odds[0].spread.current.homeOdds})</p>
                         <br/>
-                        <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
-                        <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
+                        <p>Away: {game.odds[0].moneyline.current.awayOdds}</p>
+                        <p>Home: {game.odds[0].moneyline.current.homeOdds}</p>
                         <br/>
-                        <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
-                        <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
+                        <p>Over {game.odds[0].total.current.total}: {game.odds[0].total.current.overOdds}</p>
+                        <p>Under {game.odds[0].total.current.total}: {game.odds[0].total.current.underOdds}</p>
                     </div>
                     <div className='midpoints'>
                         <h5>Starting</h5>
-                        <MidPointLines total={game.line_periods[3].period_full_game.total.total_over}/>
+                        <MidPointLines total={game.odds[0].total.current.total}/>
                     </div>
                 </div>
             ))}
