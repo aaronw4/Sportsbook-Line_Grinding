b5799983864cf29f88e8b0efa6644d337c3f464b
Aaron Williams <aaronrw4@yahoo.com>
2020-10-24 19:16:00 -0700

    Commented out baseball and MMA

--- sportsbook/src/components/gameCards.js
+++ sportsbook/src/components/gameCards.js
@@ -18,75 +18,78 @@ const GameCards = (props) => {
             game={props.game}
         />
         :
-        game === 'baseball' ?
-        <div>
-            {games.map(game => (
-                <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
-                    <div className="teams">
-                        <h5>Teams</h5>
-                        <p>{game.teams_normalized[0].name}</p>
-                        <p className='pitcher'>{game.pitcher_away.name}</p>
-                        <p>{game.teams_normalized[1].name}</p>
-                        <p className='pitcher'>{game.pitcher_home.name}</p>
-                        <p>{game.score.event_status_detail}</p>
-                    </div>
-                    <div className='betTypes'>
-                        <p className='betText'>Spread</p>
-                        <p className='betText'>Moneyline</p>
-                        <p className='betText'>Total</p>
-                    </div>
-                    <div className='currentLines'>
-                        <h5>Current</h5>
-                        <p>Away: {game.line_periods[3].period_full_game.spread.point_spread_away}({game.line_periods[3].period_full_game.spread.point_spread_away_money})</p>
-                        <p>Home: {game.line_periods[3].period_full_game.spread.point_spread_home}({game.line_periods[3].period_full_game.spread.point_spread_home_money})</p>
-                        <br/>
-                        <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
-                        <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
-                        <br/>
-                        <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
-                        <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
-                    </div>
-                    <div className='midpoints'>
-                        <h5>Starting</h5>
-                        <MidPointLines total={game.line_periods[3].period_full_game.total.total_over}/>
-                    </div>
-                </div>
-            ))}
-        </div> 
-        : game === 'MMA' ?
-        <div>
-            {games.map(game => (
-                <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
-                    <div className='teams'>
-                        <h5>Fighters</h5>
-                        <p>{game.teams_normalized[0].name}</p>
-                        <p>{game.teams_normalized[1].name}</p>
-                        <p>{game.score.event_status_detail}</p>
-                    </div>
-                    <div className='betTypes'>
-                        <p className='betText'>Moneyline</p>
-                        <p className='betText'>Total</p>
-                    </div>
-                    <div className='currentLines'>
-                        <h5>Current</h5>
-                        <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
-                        <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
-                        <br/>
-                        <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
-                        <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
-                    </div>
-                    <div className='midpoints'>
-                        <h5>Starting</h5>
-                        <MidPointLines 
-                            total={game.line_periods[3].period_full_game.total.total_over}
-                            condition2= {props.game}
-                            condition1= ''
-                        />
-                    </div>
-                </div>
-            ))}
-        </div>
-        :
+        // Baseball commented out because there is not picture data
+        // MMA commented out because API does not provide MMA data
+        // 
+        // game === 'baseball' ?
+        // <div>
+        //     {games.map(game => (
+        //         <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
+        //             <div className="teams">
+        //                 <h5>Teams</h5>
+        //                 <p>{game.teams_normalized[0].name}</p>
+        //                 <p className='pitcher'>{game.pitcher_away.name}</p>
+        //                 <p>{game.teams_normalized[1].name}</p>
+        //                 <p className='pitcher'>{game.pitcher_home.name}</p>
+        //                 <p>{game.score.event_status_detail}</p>
+        //             </div>
+        //             <div className='betTypes'>
+        //                 <p className='betText'>Spread</p>
+        //                 <p className='betText'>Moneyline</p>
+        //                 <p className='betText'>Total</p>
+        //             </div>
+        //             <div className='currentLines'>
+        //                 <h5>Current</h5>
+        //                 <p>Away: {game.line_periods[3].period_full_game.spread.point_spread_away}({game.line_periods[3].period_full_game.spread.point_spread_away_money})</p>
+        //                 <p>Home: {game.line_periods[3].period_full_game.spread.point_spread_home}({game.line_periods[3].period_full_game.spread.point_spread_home_money})</p>
+        //                 <br/>
+        //                 <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
+        //                 <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
+        //                 <br/>
+        //                 <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
+        //                 <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
+        //             </div>
+        //             <div className='midpoints'>
+        //                 <h5>Starting</h5>
+        //                 <MidPointLines total={game.line_periods[3].period_full_game.total.total_over}/>
+        //             </div>
+        //         </div>
+        //     ))}
+        // </div> 
+        // : game === 'MMA' ?
+        // <div>
+        //     {games.map(game => (
+        //         <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
+        //             <div className='teams'>
+        //                 <h5>Fighters</h5>
+        //                 <p>{game.teams_normalized[0].name}</p>
+        //                 <p>{game.teams_normalized[1].name}</p>
+        //                 <p>{game.score.event_status_detail}</p>
+        //             </div>
+        //             <div className='betTypes'>
+        //                 <p className='betText'>Moneyline</p>
+        //                 <p className='betText'>Total</p>
+        //             </div>
+        //             <div className='currentLines'>
+        //                 <h5>Current</h5>
+        //                 <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
+        //                 <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
+        //                 <br/>
+        //                 <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
+        //                 <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
+        //             </div>
+        //             <div className='midpoints'>
+        //                 <h5>Starting</h5>
+        //                 <MidPointLines 
+        //                     total={game.line_periods[3].period_full_game.total.total_over}
+        //                     condition2= {props.game}
+        //                     condition1= ''
+        //                 />
+        //             </div>
+        //         </div>
+        //     ))}
+        // </div>
+        // :
         <div>
             {games.results.map(game => (
                 <div className='bets' key={game.gameId}>
