e68ecc4af39e2d5e054fc26892cde2efad9d2efb
Aaron Williams <aaronrw4@yahoo.com>
2021-08-28 13:58:58 -0700

    Restored baseball and integrated new data source

--- sportsbook/src/components/gameCards.js
+++ sportsbook/src/components/gameCards.js
@@ -1,61 +1,68 @@
-import React from 'react'
-import MidPointLines from './lines/MidPointLines'
+import React, {useContext} from 'react'
+import { StatsContext } from '../context/statsContext';
+import MidPointLines from './MidPointLines'
 import GameCards1stHalf from './gameCards1stHalf'
 
 const GameCards = (props) => {
-    const gamePeriod = props.gamePeriod
-    const games = props.games
-    const halfPoint = props.halfPoint
-    const setHalfPoint = props.setHalfPoint
-    // const game = props.game
+    // const gamePeriod = props.gamePeriod    
+    const games = useContext(StatsContext)    
+    // const halfPoint = props.halfPoint
+    // const setHalfPoint = props.setHalfPoint
+    const game = props.game
 
     return (
-        gamePeriod === 'firstHalf' ?
-        <GameCards1stHalf
-            games={games}
-            halfPoint={halfPoint}
-            setHalfPoint={setHalfPoint}
-            game={props.game}
-        />
-        :
-        // Baseball commented out because there is not picture data
+        // gamePeriod === 'firstHalf' ?
+        // <GameCards1stHalf
+        //     games={games}
+        //     halfPoint={halfPoint}
+        //     setHalfPoint={setHalfPoint}
+        //     game={props.game}
+        // />
+        // :
         // MMA commented out because API does not provide MMA data
-        // 
-        // game === 'baseball' ?
-        // <div>
-        //     {games.map(game => (
-        //         <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
-        //             <div className="teams">
-        //                 <h5>Teams</h5>
-        //                 <p>{game.teams_normalized[0].name}</p>
-        //                 <p className='pitcher'>{game.pitcher_away.name}</p>
-        //                 <p>{game.teams_normalized[1].name}</p>
-        //                 <p className='pitcher'>{game.pitcher_home.name}</p>
-        //                 <p>{game.score.event_status_detail}</p>
-        //             </div>
-        //             <div className='betTypes'>
-        //                 <p className='betText'>Spread</p>
-        //                 <p className='betText'>Moneyline</p>
-        //                 <p className='betText'>Total</p>
-        //             </div>
-        //             <div className='currentLines'>
-        //                 <h5>Current</h5>
-        //                 <p>Away: {game.line_periods[3].period_full_game.spread.point_spread_away}({game.line_periods[3].period_full_game.spread.point_spread_away_money})</p>
-        //                 <p>Home: {game.line_periods[3].period_full_game.spread.point_spread_home}({game.line_periods[3].period_full_game.spread.point_spread_home_money})</p>
-        //                 <br/>
-        //                 <p>Away: {game.line_periods[3].period_full_game.moneyline.moneyline_away}</p>
-        //                 <p>Home: {game.line_periods[3].period_full_game.moneyline.moneyline_home}</p>
-        //                 <br/>
-        //                 <p>Over {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_over_money}</p>
-        //                 <p>Under {game.line_periods[3].period_full_game.total.total_over}: {game.line_periods[3].period_full_game.total.total_under_money}</p>
-        //             </div>
-        //             <div className='midpoints'>
-        //                 <h5>Starting</h5>
-        //                 <MidPointLines total={game.line_periods[3].period_full_game.total.total_over}/>
-        //             </div>
-        //         </div>
-        //     ))}
-        // </div> 
+        
+        game === 'baseball' ?
+        <div>
+            {games.map(game => (
+                <div className='bets' key={game.team_away + game.team_home}>
+                    <div className="teams">
+                        <h5>Teams</h5>
+                        <p>{game.team_away}</p>
+                        <p className='pitcher'>{game.pitcher_away}</p>
+                        <p>{game.team_home}</p>
+                        <p className='pitcher'>{game.pitcher_home}</p>
+                        <p>{game.time}</p>
+                    </div>
+                    <div className='betTypes'>
+                        <p className='betText'>Spread</p>
+                        <p className='betText'>Moneyline</p>
+                        <p className='betText'>Total</p>
+                    </div>
+                    <div className='currentLines'>
+                        <h5>Starting</h5>
+                        <p>Away: {game.runline_away}({game.runline_odds_away})</p>
+                        <p>Home: {game.runline_home}({game.runline_odds_home})</p>
+                        <br/>
+                        <p>Away: {game.moneyline_away}</p>
+                        <p>Home: {game.moneyline_home}</p>
+                        <br/>
+                        <p>Over {game.total}: {game.total_over}</p>
+                        <p>Under {game.total}: {game.total_under}</p>
+                    </div>
+                    <div className='midpoints'>
+                        <MidPointLines
+                            spreadAway={game.runline_odds_away}
+                            spreadHome={game.runline_odds_home}
+                            moneylineAway={game.moneyline_away}
+                            moneylineHome={game.moneyline_home}
+                            game={game.game}
+                            totalOver={game.total_over}
+                            totalUnder={game.total_under}
+                        />
+                    </div>
+                </div>
+            ))}
+        </div> 
         // : game === 'MMA' ?
         // <div>
         //     {games.map(game => (
@@ -89,7 +96,7 @@ const GameCards = (props) => {
         //         </div>
         //     ))}
         // </div>
-        // :
+        :
         <div>
             {games.map(game => (
                 game.odds ?
