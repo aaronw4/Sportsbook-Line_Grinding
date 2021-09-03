b3460609396f9d187b0f4752ea3e7424addc1107
Aaron Williams <aaronrw4@yahoo.com>
2021-09-03 15:23:37 -0700

    Made baseball1stHalf its own component

---
+++ sportsbook/src/components/baseball1stHalf.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import MidPointLines from './MidPointLines';
+
+const Baseball1stHalf = (props) => {
+    const firstHalf = props.firstHalf 
+
+    return (
+        <div>
+        {firstHalf.map(game => (
+            <div className='bets' key={game.team_away + game.team_home}>
+                <div className="teams">
+                    <h5>Teams</h5>
+                    <p>{game.team_away}</p>
+                    <p className='pitcher'>{game.pitcher_away}</p>
+                    <p>{game.team_home}</p>
+                    <p className='pitcher'>{game.pitcher_home}</p>
+                    <p>{game.time}</p>
+                </div>
+                <div className='betTypes'>
+                    <p className='betText'>Spread</p>
+                    <p className='betText'>Total</p>
+                </div>
+                <div className='currentLines'>
+                    <h5>Starting</h5>
+                    <p>Away: {game.runline_away}({game.runline_odds_away})</p>
+                    <p>Home: {game.runline_home}({game.runline_odds_home})</p>
+                    <br/>
+                    <p>Over {game.total}: {game.total_over}</p>
+                    <p>Under {game.total}: {game.total_under}</p>
+                </div>
+                <div className='midpoints'>
+                    <MidPointLines 
+                        spreadAway={game.runline_odds_away}
+                        spreadHome={game.runline_odds_home}
+                        game={game.game}
+                        totalOver={game.total_over}
+                        totalUnder={game.total_under}
+                    />
+                </div>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default Baseball1stHalf
--- sportsbook/src/components/gameCards1stHalf.js
+++ sportsbook/src/components/gameCards1stHalf.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react'
 import { StatsContext } from '../context/statsContext'
 import MidPointLines from './MidPointLines'
+import Baseball1stHalf from './baseball1stHalf'
 
 const GameCards1stHalf = (props) => { 
     const {firstHalf} = useContext(StatsContext) 
@@ -8,45 +9,7 @@ const GameCards1stHalf = (props) => {
 
     return (
         sport === 'baseball' ?
-        <div>
-            {firstHalf.map(game => (
-                <div className='bets' key={game.team_away + game.team_home}>
-                    <div className="teams">
-                        <h5>Teams</h5>
-                        <p>{game.team_away}</p>
-                        <p className='pitcher'>{game.pitcher_away}</p>
-                        <p>{game.team_home}</p>
-                        <p className='pitcher'>{game.pitcher_home}</p>
-                        <p>{game.time}</p>
-                    </div>
-                    <div className='betTypes'>
-                        <p className='betText'>Spread</p>
-                        <p className='betText'>Total</p>
-                    </div>
-                    <div className='currentLines'>
-                        <h5>Starting</h5>
-                        <p>Away: {game.runline_away}({game.runline_odds_away})</p>
-                        <p>Home: {game.runline_home}({game.runline_odds_home})</p>
-                        <br/>
-                        <p>Away: {game.moneyline_away}</p>
-                        <p>Home: {game.moneyline_home}</p>
-                        <br/>
-                        <p>Over {game.total}: {game.total_over}</p>
-                        <p>Under {game.total}: {game.total_under}</p>
-                    </div>
-                    <div className='midpoints'>
-                        <h5>Starting</h5>
-                        <MidPointLines 
-                            spreadAway={game.runline_odds_away}
-                            spreadHome={game.runline_odds_home}
-                            game={game.game}
-                            totalOver={game.total_over}
-                            totalUnder={game.total_under}
-                        />
-                    </div>
-                </div>
-            ))}
-        </div> 
+        <Baseball1stHalf firstHalf={firstHalf}/>
         :
         <div>
             {firstHalf.map(game => (
