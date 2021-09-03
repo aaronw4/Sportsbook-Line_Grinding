10770281718f75125b1051ae1f0e4f9e67fb39cc
Aaron Williams <aaronrw4@yahoo.com>
2021-09-03 14:32:43 -0700

    Restored baseball to 1st half + changed mapping

--- sportsbook/src/components/gameCards1stHalf.js
+++ sportsbook/src/components/gameCards1stHalf.js
@@ -1,65 +1,55 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { StatsContext } from '../context/statsContext'
 import MidPointLines from './MidPointLines'
 
-const GameCards1stHalf = (props) => {
-    const games = props.games
-    const halfPoint = props.halfPoint
-    // const game = props.game
+const GameCards1stHalf = (props) => { 
+    const {firstHalf} = useContext(StatsContext) 
+    const sport = props.sport
 
     return (
-        // game === 'baseball' ?
-        // <div>
-        //     {games.map(game => (
-        //         <div className='bets' key={game.line_periods[3].period_full_game.moneyline.line_id}>
-        //             <div className="teams">
-        //                 <h5>Teams</h5>
-        //                 <p>{game.teams.away.team}</p>
-        //                 <p>{game.teams.home.team}</p>
-        //                 <p>{game.status}</p>
-        //             </div>
-        //             <div className='betTypes'>
-        //                 <p className='betText'>Spread</p>
-        //                 <p className='betText'>Total</p>
-        //             </div>
-        //             <div className='currentLines'>
-        //                 <h5>Current</h5>
-        //                 <p>Away: Not provided</p>
-        //                 <p>Home: Not provided</p>
-        //                 <br/>
-        //                 <p>Away: <input 
-        //                     type='number'
-        //                     name='spread'
-        //                     value={spread}
-        //                     placeholder='Away Spread'
-        //                     onChange={handleSpread}
-        //                     />
-        //                     (Not provided)</p>
-        //                 <p>Home: {-1 * spread}(Not provided)</p>
-        //                 <br/>
-        //                 <p>Over <input 
-        //                     type='number' 
-        //                     name='total' 
-        //                     value={total}
-        //                     placeholder='Total'
-        //                     onChange={handleTotal}
-        //                     />
-        //                     : Not provided</p>
-        //                 <p>Under {total}: Not provided</p>
-        //             </div>
-        //             <div className='midpoints'>
-        //                 <h5>Starting</h5>
-        //                 <MidPointLines 
-        //                     total={total}
-        //                     condition1={props.game}
-        //                     condition2=''
-        //                 />
-        //             </div>
-        //         </div>
-        //     ))}
-        // </div> 
-        // :
+        sport === 'baseball' ?
         <div>
-            {games.map(game => (
+            {firstHalf.map(game => (
+                <div className='bets' key={game.team_away + game.team_home}>
+                    <div className="teams">
+                        <h5>Teams</h5>
+                        <p>{game.team_away}</p>
+                        <p className='pitcher'>{game.pitcher_away}</p>
+                        <p>{game.team_home}</p>
+                        <p className='pitcher'>{game.pitcher_home}</p>
+                        <p>{game.time}</p>
+                    </div>
+                    <div className='betTypes'>
+                        <p className='betText'>Spread</p>
+                        <p className='betText'>Total</p>
+                    </div>
+                    <div className='currentLines'>
+                        <h5>Starting</h5>
+                        <p>Away: {game.runline_away}({game.runline_odds_away})</p>
+                        <p>Home: {game.runline_home}({game.runline_odds_home})</p>
+                        <br/>
+                        <p>Away: {game.moneyline_away}</p>
+                        <p>Home: {game.moneyline_home}</p>
+                        <br/>
+                        <p>Over {game.total}: {game.total_over}</p>
+                        <p>Under {game.total}: {game.total_under}</p>
+                    </div>
+                    <div className='midpoints'>
+                        <h5>Starting</h5>
+                        <MidPointLines 
+                            spreadAway={game.runline_odds_away}
+                            spreadHome={game.runline_odds_home}
+                            game={game.game}
+                            totalOver={game.total_over}
+                            totalUnder={game.total_under}
+                        />
+                    </div>
+                </div>
+            ))}
+        </div> 
+        :
+        <div>
+            {firstHalf.map(game => (
                 <div className='bets' key={game.gameId}>
                     <div className='teams'>
                         <h5>Teams</h5>
